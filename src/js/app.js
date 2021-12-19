6c97aa6c249344675d09bc48e801ac7458a1dcb6
kalin0l <aleksiev2494@gmail.com>
2021-12-19 12:45:59 +0200

    job done

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,28 @@
 import "../scss/app.scss";
+const R = require('ramda');
+
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
+  let classes = R.pluck('class');
+  const getClasses = classes([
+    { name: "John", class: "is-primary" },
+    { age: 23, class: "is-warning" },
+    { job: "programmer", class: "is-danger" },
+  ]);
+  console.log(getClasses[0],getClasses[1],getClasses[2]);
   const arrayToPluck = [
     { name: "John", class: "is-primary" },
     { age: 23, class: "is-warning" },
     { job: "programmer", class: "is-danger" },
   ];
   const articles = document.querySelectorAll("article");
+
+  for(let i = 0; i<articles.length;i++){
+   for(let j = 0; j < getClasses.length;j++){
+     if(i === j) {
+       articles[i].classList.add(getClasses[j]);
+     }
+   }
+  }
 });
